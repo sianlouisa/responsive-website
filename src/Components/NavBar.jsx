3ad04b6e38aeecc1011d9d8795c45359082e52b9
SianLouisa <sianlouisa@hotmail.com>
2019-02-18 16:47:33 +0000

    nav bar with collapsing hamburger

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const NavBar = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="nav-container">
+      <span className="nav-item">Home</span>
+      <span className="nav-item">About</span>
+      <span className="nav-item">Services</span>
+      <span className="nav-item">Contact</span>
+      {/* <ul>
+        <li className="nav-item">Home</li>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Services</li>
+        <li className="nav-item">Contact</li>
+      </ul> */}
+      <span className="nav-icon">
+        <span className="nav-hamburger" />
+        <span className="nav-hamburger" />
+        <span className="nav-hamburger" />
+      </span>
+    </nav>
+  );
 };
 
 export default NavBar;
